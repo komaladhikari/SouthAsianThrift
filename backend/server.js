0eb96a41865172d2c8e98a4bcbe1cf4c98f94713
komaladh33 <160463322+jrb454@users.noreply.github.com>
2026-06-16 21:38:59 -0500

    backend set up

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-require("dotenv").config();
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 
-const app = require("./src/app");
-const connectDatabase = require("./src/shared/database/mongoose");
 
-const port = process.env.PORT || 5000;
+//app config
+const app = express()
+const port = process.env.PORT || 3000
 
-async function startServer() {
-  try {
-    await connectDatabase();
+//app middleware
+app.use(cors())
+app.use(express.json())
 
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
-}
+//api endpoints
+app.get('/',(req,res)=>{
+    res.send('API Working')
+})
 
-startServer();
+app.listen (port, ()=>{
+    console.log(`Server is running on port ${port}`)
+
+})
